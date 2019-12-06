b749d1d7d109861e6d148d8748990db68c5042a0
Roberto Cipollone <cipollone.rt@gmail.com>
2019-12-06 16:40:03 +0100

    Input pipeline.
    + Skeleton of the input pipeline ready. Loading works.
    - Missing: aspect ratio, random crop, reflection, etc.

--- artRecycle.py
+++ artRecycle.py
@@ -5,30 +5,53 @@ Main script file. Implementation of CycleGAN paper with Tensorflow 2.0.
 '''
 
 import argparse
+import tensorflow as tf
+
+import data
+
+# Debug
+from matplotlib import pyplot as plt
 
 
-
-def train():
+def train(args):
   '''\
   Training function.
+
+  Args:
+    args: namespace of arguments. Run --help for info.
   '''
 
   raise NotImplementedError()
 
 
-def use():
+def use(args):
   '''\
   Transform images with the net.
+
+  Args:
+    args: namespace of arguments. Run --help for info.
   '''
 
   raise NotImplementedError()
 
 
-def debug():
+def debug(args):
   '''\
   Debugging function.
+
+  Args:
+    args: namespace of arguments. Run --help for info.
   '''
   print('> Debug')
+
+  # Testing the input pipeline
+  dataset = data.load(*args.args, batch=2)
+  for batch in dataset:
+    for img in batch:
+      plt.imshow(tf.cast(img, dtype=tf.uint8))
+      plt.show()
+      input()
+
 
 
 def main():
@@ -36,40 +59,32 @@ def main():
   Main function. Called when this file is executed as script.
   '''
 
-  # Datasets. Identifiers and paths
-  # TODO: create consistent train/test split in txt files
-  datasets = {
-      'sisley': '../datasets/art/Alfred Sisley',
-      'guillaumin': '../datasets/art/Armand Guillaumin',
-      'caravaggio': '../datasets/art/Caravaggio',
-      'monet': '../datasets/art/Claude Monet',
-      'manet': '../datasets/art/Edouard Manet',
-      'caillebotte': '../datasets/art/Gustave Caillebotte',
-      'signac': '../datasets/art/Paul Signac',
-      'vangogh': '../datasets/art/Vincent van Gogh',
-      'foto': '../datasets/foto',
-      'summer': '../datasets/summer',
-      'winter': '../datasets/winter',
-  }
 
   ## Parsing arguments
   parser = argparse.ArgumentParser(description=
       'ArtReCycle: my implementation of CycleGAN with Tensorflow 2.0')
-  parser.add_argument('op', choices=['train','use','debug'],
-      help='What to do with the net. Most options only affect training.')
+  op_parsers = parser.add_subparsers(help='Operation', dest='op')
 
-  datasets_help = 'One of the available 
-  #parser.add_argument('-f', '--from', required=True, 
+  # Train op
+  train_parser = op_parsers.add_parser('train', help='Train the net')
+
+  # Use op
+  use_parser = op_parsers.add_parser('use',
+      help='Apply the transformation to images')
+
+  # Debug op
+  debug_parser = op_parsers.add_parser('debug')
+  debug_parser.add_argument('args', nargs='*')
 
   args = parser.parse_args()
 
   # Go
   if args.op == 'train':
-    train()
+    train(args)
   elif args.op == 'use':
-    use()
+    use(args)
   elif args.op == 'debug':
-    debug()
+    debug(args)
 
 
 if __name__ == '__main__':
---
+++ data.py
@@ -0,0 +1,106 @@
+'''\
+Data module: loading Datasets.
+
+This project uses the following convention for datases:
+Each dataset is a set of jpeg pictures in a directory. In the same directory,
+an info file, usually called 'dataset.txt', contains the list of images to
+load from that folder (some images might be excluded). This file must contain
+an empty line: this is interpreted as the separator for the training/testing
+split.
+'''
+
+import os
+import tensorflow as tf
+
+
+# Supported datasets. 'name': 'info file'
+datasets = {
+    'sisley': '../datasets/art/Alfred Sisley/dataset.txt',
+    'guillaumin': '../datasets/art/Armand Guillaumin/dataset.txt',
+    'caravaggio': '../datasets/art/Caravaggio/dataset.txt',
+    'monet': '../datasets/art/Claude Monet/dataset.txt',
+    'manet': '../datasets/art/Edouard Manet/dataset.txt',
+    'caillebotte': '../datasets/art/Gustave Caillebotte/dataset.txt',
+    'signac': '../datasets/art/Paul Signac/dataset.txt',
+    'vangogh': '../datasets/art/Vincent van Gogh/dataset.txt',
+    'foto': '../datasets/foto/dataset.txt',
+    'summer': '../datasets/summer/dataset.txt',
+    'winter': '../datasets/winter/dataset.txt',
+}
+
+
+def _dataset_files(name, split):
+  '''\
+  Returns all filenames that compose the requested dataset.
+
+  Args:
+    name: a dataset name
+    split: 'train' or 'test'
+
+  Returns:
+    Dataset of all filenames, total number of images.
+  '''
+
+  # Checks
+  if not name in datasets or not split in ('train', 'test'):
+    raise ValueError('Illegal dataset specification')
+
+  # Dataset info
+  info_file = datasets[name]
+  dataset_dir = os.path.dirname(info_file)
+  with open(info_file) as dataset_info:
+    files = dataset_info.read().splitlines()
+
+  # Split
+  for split_i in range(len(files)):
+    if len(files[split_i]) == 0:
+      break
+  if split == 'train':
+    files = files[:split_i]
+  else:
+    files = files[split_i+1:]
+
+  # Paths
+  files = [os.path.join(dataset_dir, f) for f in files]
+
+  # Dataset
+  return tf.data.Dataset.from_tensor_slices(files), len(files)
+
+
+def load(name, split, batch=None):
+  '''\
+  Returns a Dataset. The dataset is already transformed to create the input
+  pipeline.
+
+  Args:
+    name: a dataset name
+    split: 'train' or 'test'
+    batch: how many samples to return. If None, the entire dataset is returned.
+
+  Returns:
+    Tf Dataset (infinite number of elements)
+  '''
+  # TODO: incorrect image ratios. Missing random crop, flip etc
+
+  def load_image(path):
+    ''' Parses a single image. '''
+
+    img = tf.io.read_file(path)
+    img = tf.image.decode_jpeg(img, channels=3)
+    img = tf.image.resize(img, (256, 256))
+    return img
+
+  # Dataset of paths
+  images, size = _dataset_files(name, split)
+
+  # Select batch
+  if not batch or batch < 1 or batch > size :
+    batch = size
+
+  # Input pipeline
+  images = images.shuffle(min(size, 10000))
+  images = images.map(load_image)
+  images = images.batch(batch)
+  images = images.prefetch(1)
+
+  return images
